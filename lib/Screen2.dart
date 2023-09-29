d9c15a2a2298e5e34401e0c21a3af2a845894df4
Salma <11410120206031@stud.cu.edu.eg>
2023-09-30 00:49:00 +0300

    initial App

---
+++ lib/Screen2.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Screen2 extends StatelessWidget {
+  static const String routeName="Screen two";
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+      ),
+
+    );
+  }
+}
